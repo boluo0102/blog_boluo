f80160a0ccf4949073de5103d59ee4198a88c806
boluo0102 <122781673@qq.com>
2017-11-28 11:24:11 +0800

    上传头像

--- models/topic.py
+++ models/topic.py
@@ -1,5 +1,7 @@
-from models import Model
 import time
+from models import Model
+from models.user import User
+from models.mongua import Mongua
 
 
 class Topic(Model):
@@ -10,16 +12,58 @@ class Topic(Model):
         m.save()
         return m
 
-    def __init__(self,form):
+    def __init__(self, form):
         self.id = None
-        self.title = form.get('title','')
-        self.content = form.get('content','')
-        self.ct =int(time.time())
-        self.user_id = form.get('user_id', '')
         self.views = 0
-
+        self.title = form.get('title', '')
+        self.content = form.get('content', '')
+        self.ct = int(time.time())
+        self.ut = self.ct
+        self.user_id = form.get('user_id', '')
+        self.board_id = int(form.get('board_id', -1))
 
     def replies(self):
         from .reply import Reply
-        ms = Reply.find_all(topic_id = self.id)
+        ms = Reply.find_all(topic_id=self.id)
         return ms
+
+    def board(self):
+        from .board import Board
+        m = Board.find(self.board_id)
+        return m
+
+    def user(self):
+        u = User.find(id = self.user_id)
+        return u
+
+
+class Topic(Mongua):
+    __fields__ = Mongua.__fields__ + [
+        ('content', str, ''),
+        ('user_id', int, -1),
+        ('board_id', int, -1),
+        ('views', int, 0)
+    ]
+
+    @classmethod
+    def get(cls, id):
+        m = cls.find_by(id=id)
+        m.views += 1
+        m.save()
+        return m
+
+    def replies(self):
+        from .reply import Reply
+        ms = Reply.find_all(topic_id=self.id)
+        return ms
+
+    def board(self):
+        from .board import Board
+        m = Board.find(self.board_id)
+        return m
+
+    def user(self):
+        u = User.find(id = self.user_id)
+        return u
+
+
--- routes/index.py
+++ routes/index.py
@@ -5,9 +5,12 @@ from flask import (
     session,
     url_for,
     Blueprint,
-    make_response,
+    send_from_directory,
 )
 from models.user import User
+from werkzeug.utils import secure_filename
+from config import user_file_director
+import os
 
 main = Blueprint('index', __name__)
 
@@ -18,23 +21,20 @@ def current_user():
     return u
 
 
-
-
-
 @main.route("/")
 def index():
     u = current_user()
     return render_template("index.html", user=u)
 
 
-@main.route("/register",methods=['POST'])
+@main.route("/register", methods=['POST'])
 def register():
     form = request.form
     u = User.register(form)
     return redirect(url_for('.index'))
 
 
-@main.route("/login",methods=['POST'])
+@main.route("/login", methods=['POST'])
 def login():
     form = request.form
     u = User.validate_login(form)
@@ -42,7 +42,7 @@ def login():
         return redirect(url_for('.index'))
     else:
         session['user_id'] = u.id
-        session.permanent =True
+        session.permanent = True
         return redirect(url_for('topic.index'))
 
 
@@ -53,3 +53,29 @@ def profile():
         return redirect(url_for('.index'))
     else:
         return render_template('profile.html', user=u)
+
+
+def allow_file(filename):
+    suffix = filename.split('.')[-1]
+    from config import allow_list
+    return suffix in allow_list
+
+
+@main.route("/addimg", methods=["POST"])
+def addimg():
+    u = current_user()
+    if u is None:
+        return redirect(url_for('.login'))
+    else:
+        file = request.file['file']
+        filename = secure_filename(file.filename)
+        if allow_file(filename) is True:
+            file.save(os.path.join(user_file_director, filename))
+            u.user_image = filename
+            u.save()
+    return redirect(url_for(".profile"))
+
+
+@main.route('/uploads/<filename>')
+def uploads(filename):
+    return send_from_directory(user_file_director, filename)
